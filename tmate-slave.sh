1bb86c6c5e17db4c31b4ac479158ca3dbcf1707f
Pierre Carru <pierre.carru@gmail.com>
2016-09-30 18:36:48 +0200

    Add /usr/local/lib to LD_LIBRARY_PATH

--- tmate-slave.sh
+++ tmate-slave.sh
@@ -3,4 +3,4 @@ cd /tmate-slave
 if [ -n "${HOST}" ]; then
   hostopt="-h ${HOST}"
 fi
-./tmate-slave $hostopt -p ${PORT?2222} 2>&1
+LD_LIBRARY_PATH=/usr/local/lib ./tmate-slave $hostopt -p ${PORT?2222} 2>&1
